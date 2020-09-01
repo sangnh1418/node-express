377335c1d2a2aab715397759d896cce8a7f9ccdc
sangnh <sangnh@solazu.com>
2020-09-01 21:39:00 +0700

    Query parameters

--- 1-start-node-express/index.js
+++ 1-start-node-express/index.js
@@ -10,22 +10,31 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'node-express-coder-tokyo', message: 'Node Express' })
 })
 
+const users = [
+  {
+    name: "Sang",
+    age: 20,
+  },
+  {
+    name: "Son",
+    age: 20,
+  },
+  {
+    name: "Quang",
+    age: 20,
+  }
+]
 app.get('/users', (req, res) => {
   res.render('users/index', {
-    users: [
-      {
-        name: "Sang",
-        age: 20,
-      },
-      {
-        name: "Son",
-        age: 20,
-      },
-      {
-        name: "Quang",
-        age: 20,
-      }
-    ]
+    users
+  })
+})
+
+app.get('/users/search', (req, res) => {
+  const { q } = req.query
+  const matchUser = users.filter((user) => user.name.indexOf(q) !== -1)
+  res.render('users/index', {
+    users: matchUser
   })
 })
 
