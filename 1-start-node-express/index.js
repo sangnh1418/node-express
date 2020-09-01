b5ad7092a820dfac6193874b36b83a4fb7a2e438
sangnh <sangnh@solazu.com>
2020-09-01 22:36:14 +0700

    Post method

--- 1-start-node-express/index.js
+++ 1-start-node-express/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 5000
 
 app.set('views', './views')
 
 app.set('view engine', 'pug')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'node-express-coder-tokyo', message: 'Node Express' })
@@ -36,6 +41,15 @@ app.get('/users/search', (req, res) => {
   res.render('users/index', {
     users: matchUser
   })
+})
+
+app.get('/users/create', (req, res) => {
+  res.render('users/create')
+})
+
+app.post('/users/create', (req, res) => {
+  users.push(req.body)
+  res.redirect('/users')
 })
 
 app.listen(port, () => {
