6621fa9e7f48754e38b42592f919fe04fae30756
sangnh <sangnh@solazu.com>
2020-09-01 21:10:38 +0700

    Template engines

--- 1-start-node-express/index.js
+++ 1-start-node-express/index.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.set('views', './views')
+
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Node Express!</h1>')
+  res.render('index', { title: 'node-express-coder-tokyo', message: 'Node Express' })
+})
+
+app.get('/users', (req, res) => {
+  res.render('users/index', {
+    users: [
+      {
+        name: "Sang",
+        age: 20,
+      },
+      {
+        name: "Son",
+        age: 20,
+      },
+      {
+        name: "Quang",
+        age: 20,
+      }
+    ]
+  })
 })
 
 app.listen(port, () => {
