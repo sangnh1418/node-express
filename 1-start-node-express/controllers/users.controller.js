e0c7026dc9c3ba48c8c7e06bd18d2ca3be16122d
sangnh <sangnh@solazu.com>
2020-09-02 10:35:06 +0700

    Template layout

---
+++ 1-start-node-express/controllers/users.controller.js
@@ -0,0 +1,36 @@
+const db = require("../db")
+const shortid = require('shortid')
+
+module.exports.index = (req, res) => {
+  res.render('users/index', {
+    users: db.get("users").value()
+  })
+}
+
+module.exports.search = (req, res) => {
+  const { q } = req.query
+  const matchUser = db.get("users").value().filter((user) => user.name.indexOf(q) !== -1)
+  res.render('users/index', {
+    users: matchUser
+  })
+}
+
+module.exports.id = (req, res) => {
+  const id = req.params.id
+  const user = db.get("users").find({ id: id }).value()
+  res.render("users/view", {
+    user: user
+  })
+}
+
+module.exports.create = (req, res) => {
+  res.render('users/create')
+}
+
+module.exports.postCreate = (req, res) => {
+  db.get("users").push({
+    id: shortid.generate(),
+    ...req.body
+  }).write()
+  res.redirect('/users')
+}
