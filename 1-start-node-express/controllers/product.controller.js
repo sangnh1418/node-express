3a57f1cace37a361fc92e52b43ba121f3e9195a0
sangnh <sangnh@solazu.com>
2020-09-03 15:46:29 +0700

    pagination products

--- 1-start-node-express/controllers/product.controller.js
+++ 1-start-node-express/controllers/product.controller.js
@@ -7,8 +7,14 @@ module.exports.products = (req, res) => {
   const start = (page - 1) * perPage
   const end = page * perPage
 
+  let pagination = []
+
+  pagination.push(page, page + 1, page + 2)
+
   const productData = db.get("products").value()
   res.render('products/index', {
-    productData: productData.slice(start, end)
+    productData: productData.slice(start, end),
+    pagination: pagination,
+    activePagination: page
   })
 }
